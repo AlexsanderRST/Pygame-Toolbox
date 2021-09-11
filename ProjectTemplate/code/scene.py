cfd1a1e69ebd5b8e761cba0dc370258dcab4a157
Alexsander Rosante <xxalexcreeperxx@gmail.com>
2021-09-10 22:00:39 -0300

    Project Template added

---
+++ ProjectTemplate/code/scene.py
@@ -0,0 +1,22 @@
+"""Alexsander Rosante's creation"""
+
+import pygame
+from pygame.locals import *
+from settings import *
+
+
+class Basic:
+    def __init__(self):
+        pass
+
+    def event_check(self, events):
+        for event in events:
+            pass
+
+    def run(self, screen, events):
+        # methods
+
+        # updates
+
+        # draws
+        screen.fill('#1b1464')
---
+++ ProjectTemplate/code/settings.py
@@ -0,0 +1,10 @@
+"""Alexsander Rosante's creation"""
+
+game_name = 'My Pygame'
+game_creator = 'Alexsander Rosante'
+game_version = '0.1'
+
+screen_width = 1024
+screen_height = 576
+screen_w = screen_width
+screen_h = screen_height
